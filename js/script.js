c91d0f99b133b4ff106486811735e18ca3d12a6e
Jay Koshti <jay.koshti@simformsolutions.com>
2023-03-16 12:57:30 +0530

    added function addProduct() to add product dynamic

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ function tableCreate(map) {
         document.body.removeChild(document.getElementById("display_prd_color"));
     }
 
-    if(map.size===0) {
+    if (map.size === 0) {
         alert("Please select the product color first...");
         return false;
     }
@@ -109,27 +109,186 @@ function fetchProductColors() {
 }
 
 function addProduct() {
-    const _prdName = document.getElementById('prdName');
+    const _prdName = document.getElementById('prdName').value;
 
-    if(_prdName.value==='') {
+    document.getElementById('prdName').value = "";
+
+    if (_prdName === '') {
         alert('Please add the product name ...');
         return false;
     }
-    else if(localStorage.getItem('prdNames')===null && prdNames.length===0) {
-        prdNames.push(_prdName.value);
+    else if (localStorage.getItem('prdNames') === null && prdNames.length === 0) {
+        prdNames.push(_prdName);
+        localStorage.setItem('prdNames', prdNames);
     }
-    else if(localStorage.getItem('prdNames')!==null) {
-         prdNames = Array(localStorage.getItem('prdNames'));
-         prdNames.push(_prdName.value);
+    else if (localStorage.getItem('prdNames') !== null) {
+        let prdArr = Array(localStorage.getItem('prdNames'));
+       
+        for(let i=0; i<prdArr.length; i++) {
+            prdNames[i] = prdArr[i];
+        }
+        prdNames.push(_prdName);
+        localStorage.setItem('prdNames', prdNames);
     }
 
-    localStorage.setItem('prdNames', prdNames);
+    displayProduct();
+}
 
- 
+function displayProduct() {
+
+    let prod_container = document.getElementById("prd_container");
+    
+    let divRow = document.createElement("div");
+    divRow.className = "row";
+
+    prdArr = Array(localStorage.getItem('prdNames'));
+
+    console.log(prdArr);
+
+    // for (let prd of prdArr) {
+
+        // CREATED FIRST COL
+        let divCol1 = document.createElement("div");
+        divCol1.className = "col py-1";
+        divCol1.style.textAlign = 'center';
+
+        let colContent1 = document.createTextNode(prdNames[prdNames.length-1]);
+        divCol1.appendChild(colContent1);
+
+        // CREATED SECOND COL
+        let divCol2 = document.createElement("div");
+        divCol2.className = "col";
+
+        let divCol2Row = document.createElement("div");
+        divCol2Row.className = "row";
+
+        // first radio button for red color
+        let divCol2Row_Col1 = document.createElement("div");
+        divCol2Row_Col1.className = "col";
+
+        let formCheckDiv1 = document.createElement("div");
+        formCheckDiv1.className = "form-check";
+
+        let radioBtn1 = document.createElement("input");
+        radioBtn1.className = "form-check-input";
+        radioBtn1.setAttribute("type", "radio");
+        radioBtn1.setAttribute("value", "red");
+        radioBtn1.setAttribute("id", "red_color");
+
+        formCheckDiv1.appendChild(radioBtn1);
+        divCol2Row_Col1.appendChild(formCheckDiv1);
+
+        // second radio button for green color
+        let divCol2Row_Col2 = document.createElement("div");
+        divCol2Row_Col2.className = "col";
+
+        let formCheckDiv2 = document.createElement("div");
+        formCheckDiv2.className = "form-check";
+
+        let radioBtn2 = document.createElement("input");
+        radioBtn2.className = "form-check-input";
+        radioBtn2.setAttribute("type", "radio");
+        radioBtn2.setAttribute("value", "green");
+        radioBtn2.setAttribute("id", "green_color");
+
+        formCheckDiv2.appendChild(radioBtn2);
+        divCol2Row_Col2.appendChild(formCheckDiv2);
+
+        // third radio button for yellow color
+        let divCol2Row_Col3 = document.createElement("div");
+        divCol2Row_Col3.className = "col";
+
+        let formCheckDiv3 = document.createElement("div");
+        formCheckDiv3.className = "form-check";
+
+        let radioBtn3 = document.createElement("input");
+        radioBtn3.className = "form-check-input";
+        radioBtn3.setAttribute("type", "radio");
+        radioBtn3.setAttribute("value", "yellow");
+        radioBtn3.setAttribute("id", "yellow_color");
+
+        formCheckDiv3.appendChild(radioBtn3);
+        divCol2Row_Col3.appendChild(formCheckDiv3);
+
+        // fourth radio button for blue color
+        let divCol2Row_Col4 = document.createElement("div");
+        divCol2Row_Col4.className = "col";
+
+        let formCheckDiv4 = document.createElement("div");
+        formCheckDiv4.className = "form-check";
+
+        let radioBtn4 = document.createElement("input");
+        radioBtn4.className = "form-check-input";
+        radioBtn4.setAttribute("type", "radio");
+        radioBtn4.setAttribute("value", "blue");
+        radioBtn4.setAttribute("id", "blue_color");
+
+        formCheckDiv4.appendChild(radioBtn4);
+        divCol2Row_Col4.appendChild(formCheckDiv4);
+
+        // fifth radio button for orange color
+        let divCol2Row_Col5 = document.createElement("div");
+        divCol2Row_Col5.className = "col";
+
+        let formCheckDiv5 = document.createElement("div");
+        formCheckDiv5.className = "form-check";
+
+        let radioBtn5 = document.createElement("input");
+        radioBtn5.className = "form-check-input";
+        radioBtn5.setAttribute("type", "radio");
+        radioBtn5.setAttribute("value", "orange");
+        radioBtn5.setAttribute("id", "orange_color");
+
+        formCheckDiv5.appendChild(radioBtn5);
+        divCol2Row_Col5.appendChild(formCheckDiv5);
+
+        // sixth radio button for black color
+        let divCol2Row_Col6 = document.createElement("div");
+        divCol2Row_Col6.className = "col";
+
+        let formCheckDiv6 = document.createElement("div");
+        formCheckDiv6.className = "form-check";
+
+        let radioBtn6 = document.createElement("input");
+        radioBtn6.className = "form-check-input";
+        radioBtn6.setAttribute("type", "radio");
+        radioBtn6.setAttribute("value", "black");
+        radioBtn6.setAttribute("id", "black_color");
+
+        formCheckDiv6.appendChild(radioBtn6);
+        divCol2Row_Col6.appendChild(formCheckDiv6);
+
+        divCol2Row.appendChild(divCol2Row_Col1);
+        divCol2Row.appendChild(divCol2Row_Col2);
+        divCol2Row.appendChild(divCol2Row_Col3);
+        divCol2Row.appendChild(divCol2Row_Col4);
+        divCol2Row.appendChild(divCol2Row_Col5);
+        divCol2Row.appendChild(divCol2Row_Col6);
+
+        // CREATED THIRD COL
+        let divCol3 = document.createElement("div");
+        divCol3.className = "col py-2";
+        divCol3.style.textAlign = 'center';
+
+        let delBtn = document.createElement("button");
+        delBtn.setAttribute('type', 'button');
+        delBtn.className = "btn btn-primary";
+       
+
+        let BtnContent = document.createTextNode("delete");
+        delBtn.appendChild(BtnContent);
+
+        divCol3.appendChild(delBtn);
+
+        divRow.appendChild(divCol1);
+        divRow.appendChild(divCol2);
+        divRow.appendChild(divCol3);
+
+
+        prod_container.appendChild(divRow);
+
+    // }
 }
 
 function reloadModal() {
-    if(document.getElementsByClassName('prd-name').value!=='') {
-        document.getElementsByClassName('prd-name').value = '';
-    }
 }
