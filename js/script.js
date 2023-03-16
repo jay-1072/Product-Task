56dee3749731f2fe50988f2dd4295db56c6e4169
Jay Koshti <jay.koshti@simformsolutions.com>
2023-03-16 11:18:26 +0530

    added modal that store data in local storage

--- js/script.js
+++ js/script.js
@@ -1,10 +1,75 @@
-function fetchProductColors() {
-    // const ele = document.getElementsByName('prd4_color');
 
-    // for (i = 0; i < ele.length; i++) {
-    //     if (ele[i].checked)
-    //         document.getElementById("sjib").innerHTML = ele[i].value;
-    // }
+var prdNames = [];
+
+function tableCreate(map) {
+
+    if (document.getElementById("display_prd_color")) {
+        document.body.removeChild(document.getElementById("display_prd_color"));
+    }
+
+    if(map.size===0) {
+        alert("Please select the product color first...");
+        return false;
+    }
+
+    const div = document.createElement('div');
+    div.setAttribute("class", "container")
+    div.setAttribute("id", "display_prd_color");
+    div.style.marginTop = "20px";
+    div.style.padding = "0";
+
+    const body = document.body, tbl = document.createElement('table');
+    tbl.setAttribute("id", "prdColorTbl");
+    tbl.style.width = '200px';
+    tbl.style.border = '1px solid black';
+    tbl.style.marginBottom = '50px';
+
+    const header = document.createElement('thead');
+
+    const headingRow = tbl.insertRow();
+
+    const headingCell1 = headingRow.insertCell();
+    headingCell1.appendChild(document.createTextNode(`Product color`));
+    headingCell1.style.border = '1px solid black';
+    headingCell1.style.fontWeight = '700'
+
+    const headingCell2 = headingRow.insertCell();
+    headingCell2.appendChild(document.createTextNode(`Color count`));
+    headingCell2.style.border = '1px solid black';
+    headingCell2.style.fontWeight = '700'
+
+    headingRow.appendChild(headingCell1);
+    headingRow.appendChild(headingCell2);
+
+    header.appendChild(headingRow);
+    tbl.appendChild(header);
+
+    const key_itr = map.keys();
+    const val_itr = map.values();
+
+    for (let i = 0; i < map.size; i++) {
+        const tr = tbl.insertRow();
+        for (let j = 0; j < 1; j++) {
+
+            const td1 = tr.insertCell();
+            td1.appendChild(document.createTextNode(key_itr.next().value));
+            td1.style.border = '1px solid black';
+            td1.style.textAlign = 'center';
+
+            const td2 = tr.insertCell();
+            td2.appendChild(document.createTextNode(val_itr.next().value));
+            td2.style.border = '1px solid black';
+            td2.style.textAlign = 'center';
+        }
+    }
+
+    if (map.size !== 0) {
+        div.appendChild(tbl);
+        body.appendChild(div);
+    }
+}
+
+function fetchProductColors() {
 
     let redCnt = 0, greenCnt = 0, yellowCnt = 0, blueCnt = 0, orangeCnt = 0, blackCnt = 0;
     const map = new Map();
@@ -40,67 +105,31 @@ function fetchProductColors() {
         }
     }
 
-    function tableCreate() {
+    tableCreate(map);
+}
 
-        if(document.getElementById("display_prd_color")) {
-            document.body.removeChild(document.getElementById("display_prd_color"));
-        }
+function addProduct() {
+    const _prdName = document.getElementById('prdName');
 
-        const div = document.createElement('div');
-        div.setAttribute("class", "container")
-        div.setAttribute("id", "display_prd_color");
-        div.style.marginTop = "20px";
-        div.style.padding = "0";
-        
-
-        const body = document.body, tbl = document.createElement('table');
-        tbl.setAttribute("id", "prdColorTbl");
-    
-        tbl.style.width = '200px';
-        tbl.style.border = '1px solid black';
-        tbl.style.marginBottom = '50px';
-
-        const header = document.createElement('thead');
-
-        const headingRow = tbl.insertRow();
-
-        const headingCell1 = headingRow.insertCell();
-        headingCell1.appendChild(document.createTextNode(`Product color`));
-        headingCell1.style.border = '1px solid black';
-        headingCell1.style.fontWeight = '700'
-        
-        const headingCell2 = headingRow.insertCell();
-        headingCell2.appendChild(document.createTextNode(`Color count`));
-        headingCell2.style.border = '1px solid black';
-        headingCell2.style.fontWeight = '700'
-
-        headingRow.appendChild(headingCell1);
-        headingRow.appendChild(headingCell2);
-
-        header.appendChild(headingRow);
-        tbl.appendChild(header);
-    
-        const key_itr = map.keys();
-        const val_itr = map.values();
-
-        for (let i = 0; i < map.size; i++) {
-            const tr = tbl.insertRow();
-            for (let j = 0; j < 1; j++) {
-
-                const td1 = tr.insertCell();
-                td1.appendChild(document.createTextNode(key_itr.next().value));
-                td1.style.border = '1px solid black';
-                td1.style.textAlign = 'center';
-
-                const td2 = tr.insertCell();
-                td2.appendChild(document.createTextNode(val_itr.next().value));
-                td2.style.border = '1px solid black';
-                td2.style.textAlign = 'center';
-            }
-        }
-        div.appendChild(tbl);
-        body.appendChild(div);
+    if(_prdName.value==='') {
+        alert('Please add the product name ...');
+        return false;
+    }
+    else if(localStorage.getItem('prdNames')===null && prdNames.length===0) {
+        prdNames.push(_prdName.value);
+    }
+    else if(localStorage.getItem('prdNames')!==null) {
+         prdNames = Array(localStorage.getItem('prdNames'));
+         prdNames.push(_prdName.value);
     }
 
-    tableCreate();
+    localStorage.setItem('prdNames', prdNames);
+
+ 
+}
+
+function reloadModal() {
+    if(document.getElementsByClassName('prd-name').value!=='') {
+        document.getElementsByClassName('prd-name').value = '';
+    }
 }
